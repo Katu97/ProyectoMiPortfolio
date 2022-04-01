07876df372dd6ef02ab37aa9d3899ca5b5f530f3
Katu97 <skatuchin.97.sk@gmail.com>
2022-04-01 00:58:08 -0300

    retoques acerca de mi

--- app.js
+++ app.js
@@ -49,3 +49,20 @@ const active = document.getElementsByClassName("active");
 active.addEventListener("click",() => {
     document.menu.nav.ul.li.a.classList.toggle("selected");
 });*/
+
+var animateButton = function (e) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove("animate");
+  
+    e.target.classList.add("animate");
+    setTimeout(function () {
+      e.target.classList.remove("animate");
+    }, 700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener("click", animateButton, false);
+  }
